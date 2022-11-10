b8cd13d320e0112a983f646680ca6cf9ea40926e
cutevf17@gmail.com <cutevf17@gmail.com>
2022-11-10 23:50:59 +0300

    modified:   lab12.c 	modified:   lab12.c

--- lab12.c
+++ lab12.c
@@ -1,8 +1,14 @@
+
 #include <stdio.h>
 
 #define SIGN 0
 #define NUM 1
 #define CONTINUE 2
+#define RUBBISH 3
+
+int Abs(int n){
+    return n = (n > 0) ? n : -n;
+}
 
 void DoubOutput(char doub[]) {
     for (int i = 0; i < 8; i++) {
@@ -10,22 +16,21 @@ void DoubOutput(char doub[]) {
     }
 }
 
-int ToDouble(char num, char sign) {
-    int intnum = num, i = 1, flag = 0;
+void ToDouble(char num, char sign) {
+    int i = 1;
     char doub[8] = "00000000";
-    while (intnum > 0) {
-        doub[8-i] = (intnum % 2) + '0';
-        intnum /= 2;
-        ++i; 
+    while (Abs(num) > 0 && i <= 8) {
+        doub[8-i] = (Abs(num) % 2) + '0';
+        num = num / 2;
+        ++i;
     }
     if (sign == '+') {
        DoubOutput(doub); 
     } else {
-        doub[0] = '1';
-        for (int i = 1; i < 8; ++i) {
+        for (int i = 0; i < 8; ++i) {
             doub[i] = (doub[i] == '0') ? '1' : '0';
         }
-        for (int i = 7; i > 1; --i) {
+        for (int i = 7; i >= 0; --i) {
             if (doub[i] == '0') {
                 doub[i] = '1';
                 break;
@@ -33,33 +38,49 @@ int ToDouble(char num, char sign) {
                 doub[i] = '0';
             }
         }
+        doub[0] = '1';
         DoubOutput(doub);
     }
     putchar('\n');
 }
 
 int StatusUpdate(char ch, char *sign){
-    if (ch >= '0' && ch <= '9') {
+    if ((ch >= '0') && (ch <= '9')) {
         return NUM;
-    } else if (ch == '-') {
+    } else if (ch = '-') {
         *sign = '-';
         return SIGN;
     } else {
         return CONTINUE;
     }
+
 }
 
-char ClayNum(char ch, int k, char num){
-    if (k == 0) {
-        num += ch - '0';
-    } else if (k == 1) {
-        num *= 10;
-        num += ch - '0';
-    } else if (k == 2) {
-        num *= 10;
-        num += ch - '0';
+char ClayNum(char ch, int k, char num, char sign){
+    if (sign == '+'){
+        if (k == 0) {
+            num += ch - '0';
+        } else if (k == 1) {
+            num *= 10;
+            num += ch - '0';
+        } else if (k == 2) {
+            num *= 10;
+            num += ch - '0';
+        }
+    }
+    if (sign == '-'){
+        if (k == 0) {
+            num -= ch - '0';
+        } else if (k == 1) {
+            num *= 10;
+            num -= ch - '0';
+        } else if (k == 2) {
+            num *= 10;
+            num -= ch - '0';
+        }
     }
     return num;
+
 }
 
 int main() {
@@ -67,22 +88,20 @@ int main() {
     int state = CONTINUE;
     int k = 0;
     while ((ch = getchar()) != EOF){
-
         state = StatusUpdate(ch, &sign);
-
         if (state == NUM) {
-            num = ClayNum(ch, k, num);
-            ++k;
-        } else if (state == CONTINUE) {
-            if (num < 0) {
-                putchar('!');
-                putchar('\n');
+            num = ClayNum(ch, k, num, sign);
+            ++k;  
+        } else {
+           if (k != 0){
+               ToDouble(num, sign);
+               printf("%d\n", num);
             } else {
-                ToDouble(num, sign);
+                putchar(ch);
             }
-        num = ' ' - 32;
-        k = 0;
-        sign = '+';
+            num = ' ' - 32;
+            sign = '+';
+            k = 0; 
         }
     }
 }
