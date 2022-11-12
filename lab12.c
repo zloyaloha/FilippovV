c5aae4cefefb0b465a28e7bae31ef0a7b6e32619
cutevf17@gmail.com <cutevf17@gmail.com>
2022-11-12 16:23:24 +0300

    something new

--- lab12.c
+++ lab12.c
@@ -5,40 +5,39 @@
 #define CONTINUE 2
 #define END 3
 
-int Abs(int n) {
+char Abs(char n) {
     return n = (n > 0) ? n : -n;
 }
 
-void DoubOutput(char doub[]) {
-    for (int i = 0; i < 8; i++) {
-        putchar(doub[i]);
+void RecursionOutput(char num) {
+    if (Abs(num) > 0) {
+        RecursionOutput(Abs(num) / 2);
+        putchar('0' + Abs(num) % 2);
     }
 }
 
-void ToDouble(char num, char sign) {
-    int i = 1;
-    char doub[8] = "00000000";
-    while (Abs(num) > 0) {
-        doub[8-i] = (Abs(num) % 2) + '0';
-        num /= 2;
-        ++i;
+char Pow(char num, char power) {
+    char res = 1;
+    for (int i = 1; i <= power; i++) {
+        res *= num;
     }
-    if (sign == '-') {
-       for (int i = 0; i < 8; ++i) {
-            doub[i] = (doub[i] == '0') ? '1' : '0';
-        }
-        for (int i = 7; i > 0; --i) {
-            if (doub[i] == '0') {
-                doub[i] = '1';
-                break;
-            } else {
-                doub[i] = '0';
-            }
-        }
-        doub[0] = '1';
-    } 
-     DoubOutput(doub);   
-    putchar('\n');
+    return res;
+}
+
+char ClosetPowerOfTwo(char num){
+    char n = Abs(num);
+    unsigned char base = 1;
+    char power = 0;
+    while (base < n) {
+        base = base * 2;
+        power += 1;
+    }
+    return power;
+}
+
+
+int AdditionalCode (char num) {
+    return Pow(2, ClosetPowerOfTwo(num)) - num; 
 }
 
 int StatusUpdate(char ch, char *sign){
@@ -47,9 +46,8 @@ int StatusUpdate(char ch, char *sign){
     } else if (ch == '-') {
         *sign = '-';
         return SIGN;
-    } else {
-        return CONTINUE;
     }
+    return CONTINUE;
 }
 
 char ClayNum(char ch, int k, char num, char sign){
@@ -77,6 +75,32 @@ char ClayNum(char ch, int k, char num, char sign){
     return num;
 }
 
+void DobeyEgo(char num, char sign) {
+    for (int i = 0; i < 6 - ClosetPowerOfTwo(num); ++i){
+        if (sign == '+') {
+            putchar('1');
+        } else {
+            putchar('0');
+        }
+    }
+}
+
+int Exeptions(char num){
+    if (num == -128) {
+        putchar('1');
+        for (int i = 0; i < 7; i++){
+            putchar('0');
+        }
+    } else if (num == 0) {
+        for (int i = 0; i < 8; i++){
+            putchar('0');
+        }
+    } else {
+        return 1;
+    }
+    return 0; 
+}
+
 int main() {
     char ch, sign = '+', num = 0;
     int state = CONTINUE;
@@ -88,13 +112,28 @@ int main() {
             num = ClayNum(ch, k, num, sign);
             ++k;
         } else if (state == CONTINUE && k > 0) {
-            ToDouble(num, sign);
-            num = ' ' - 32;
-            k = 0;
-            sign = '+';
+            if (num == -128) {
+                Exeptions(num);
+            } else if (num == 0) {
+                Exeptions(num);
+            } else {
+                if (sign == '-') {
+                    putchar('1');
+                    num = AdditionalCode(Abs(num));
+                } else {
+                    putchar('0');
+                }
+                DobeyEgo(Abs(num), sign);
+                RecursionOutput(num);
+            }
+        putchar('\n');
+        k = 0;
+        sign = '+';
+        num = 0;
         }
-        if (ch == EOF) {
+        if (ch == EOF){
             break;
         }
-    }    
-}
+    }
+}    
+
